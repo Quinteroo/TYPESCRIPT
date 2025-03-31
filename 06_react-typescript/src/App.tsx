3e1b9702f615c169a3477ef7787fab41d44a1e86
quintero git <hello.quinteroo@gmail.com>
2025-03-31 19:14:44 +0200

    UPDATE 06_react-typescript taildwind config

---
+++ 06_react-typescript/src/App.tsx
@@ -0,0 +1,17 @@
+
+
+
+function App() {
+
+
+  return (
+    <>
+    <div className='flex flex-col items-center justify-center h-svh w-full'>
+      <h1 className='text-4xl font-bold'>React + TS</h1>
+     
+    </div>
+    </>
+  )
+}
+
+export default App
