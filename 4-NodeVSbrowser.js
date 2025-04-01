cc2f6c0ae26ad7eb7e9401609549a4410cc318bd
qugodev <146204443+Quinteroo@users.noreply.github.com>
2025-04-01 16:22:16 +0200

    Create 4-NodeVSbrowser.js

---
+++ 4-NodeVSbrowser.js
@@ -0,0 +1,18 @@
+
+
+//! DIFERENCIAS ENTRE NAVEGADOR Y NODE.JS
+Tenemos que tener en cuenta que es una gran ventaja que podamos usar el mismo lenguaje tanto en el frontend como en el backend,
+esto reduce el tiempo y lo duro que resulta aprender un nuevo lenguaje, además de facilitar la comunicación entre los equipos.
+
+Además, aunque existan diferencia en el manejo de modulos, Node.js soporta también JS moderno, pudiendo escribirlo en el servidor,
+en cambio, los navegadores suelen tardar un poco en actualizarse, lo que relentiza el uso.
+
+
+📌 Diferencias entre Node.js y el navegador
+1️⃣ Entorno de ejecución (uno es el navegador y otro el servidor/backend)
+2️⃣ Manejo de módulos (require en Node.js vs import en el navegador)
+3️⃣ APIs disponibles (Node.js tiene acceso a archivos, red, procesos, mientras que el navegador tiene DOM, fetch, localStorage)
+4️⃣ Manejo de eventos (EventEmitter en Node.js vs addEventListener en el navegador)
+5️⃣ Seguridad (Node.js tiene acceso al sistema de archivos, mientras que el navegador está restringido por el sandbox)
+6️⃣ Uso de JavaScript (Node.js ejecuta código backend, el navegador ejecuta código frontend)
+7️⃣ Ejemplo de ejecución (Node.js usa console.log, el navegador usa document.write o manipula el DOM)
