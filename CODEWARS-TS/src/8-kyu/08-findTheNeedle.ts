3193c9f0e36c42fcbfefcb31cc32ed9df879de63
qugodev <146204443+Quinteroo@users.noreply.github.com>
2025-04-02 12:55:12 +0200

    Create 08-findTheNeedle.ts

---
+++ CODEWARS-TS/src/8-kyu/08-findTheNeedle.ts
@@ -0,0 +1,26 @@
+
+
+
+//! A Needle in the Haystack
+//? https://www.codewars.com/kata/56676e8fabd2d1ff3000000c/train/typescript
+
+//Can you find the needle in the haystack?
+//Write a function findNeedle() that takes an array full of junk but containing one "needle"
+//After your function finds the needle it should return a message (as a string) that says:
+//"found the needle at position " plus the index it found the needle, so:
+
+//Example(Input --> Output)
+//["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"] --> "found the needle at position 5" 
+
+export function findNeedle(haystack: any[]):string {
+
+    const needlePosition = haystack.findIndex((e)=> e === "needle")
+
+    return `found the needle at position ${needlePosition}`
+}
+
+
+//? BEST PRACTICES
+export function findNeedle(haystack: any[]):string {
+  return "found the needle at position " + haystack.indexOf('needle');
+}
