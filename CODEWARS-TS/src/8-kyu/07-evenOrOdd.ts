7093046ee34299ce6c42000399a905644f4bc765
qugodev <146204443+Quinteroo@users.noreply.github.com>
2025-04-02 12:36:50 +0200

    Create 07-evenOrOdd.ts

---
+++ CODEWARS-TS/src/8-kyu/07-evenOrOdd.ts
@@ -0,0 +1,10 @@
+
+
+//! Even or Odd
+//? https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/typescript
+
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+export function evenOrOdd(n:number):string {
+  return (n%2 !==0)?"Odd":"Even";
+}
