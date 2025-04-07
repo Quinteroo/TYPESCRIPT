b4cafa61b841aa1e1e445e2fb754fb4cf8ed2f75
qugodev <146204443+Quinteroo@users.noreply.github.com>
2025-04-07 15:52:05 +0200

    Create 21-helloWorld.ts

---
+++ CODEWARS-TS/src/8-kyu/21-helloWorld.ts
@@ -0,0 +1,11 @@
+
+
+
+//! Function 1 - hello world
+//? https://www.codewars.com/kata/523b4ff7adca849afe000035/train/typescript
+
+// Make a simple function called greet that returns the most-famous "hello world!".
+
+export function greet():string{
+  return "hello world!"
+}
