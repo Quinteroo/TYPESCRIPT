bc0a8edae1d53d4bef0e968023d647ed140b44fe
quintero git <hello.quinteroo@gmail.com>
2025-04-12 11:41:57 +0200

    UPDATE 8-kyu CODEWARS

---
+++ CODEWARS-TS/src/8-kyu/27-oppositeNumber.ts
@@ -0,0 +1,20 @@
+
+
+
+//! Opposite number
+//? https://www.codewars.com/kata/56dec885c54a926dcd001095/train/typescript
+
+// Very simple, given a number (integer / decimal / both depending on the language), find its opposite (additive inverse).
+
+// Examples:
+
+// 1: -1
+// 14: -14
+// -34: 34
+
+
+export class Kata {
+  static opposite(n: number) {
+    return n * -1
+  }
+}
