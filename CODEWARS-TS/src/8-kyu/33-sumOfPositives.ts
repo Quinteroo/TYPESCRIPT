ecdbcbf9767e6fb2122135ad6076e3f23eabbbf2
qugodev <146204443+Quinteroo@users.noreply.github.com>
2025-04-15 14:59:52 +0200

    Update 33-sumOfPositives.ts

--- CODEWARS-TS/src/8-kyu/33-sumOfPositives.ts
+++ CODEWARS-TS/src/8-kyu/33-sumOfPositives.ts
@@ -24,3 +24,9 @@ export function positiveSum(arr:number[]| null):number {
 
   return total;
 }
+
+
+//! REFACTORIZADO
+export function positiveSum(arr:number[]):number {
+  return arr.filter((e) => e >= 0).reduce((acc, e) => acc + e , 0)
+}
