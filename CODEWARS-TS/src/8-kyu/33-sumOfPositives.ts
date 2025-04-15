8f772a859bd11f971662a05aaede24c3211134cf
qugodev <146204443+Quinteroo@users.noreply.github.com>
2025-04-15 14:45:35 +0200

    Create 33-sumOfPositives.ts

---
+++ CODEWARS-TS/src/8-kyu/33-sumOfPositives.ts
@@ -0,0 +1,26 @@
+
+
+
+//! Sum of positive
+//? https://www.codewars.com/kata/5715eaedb436cf5606000381/train/typescript
+
+//Task
+//You get an array of numbers, return the sum of all of the positives ones.
+
+//Example
+//[1, -4, 7, 12] => 20
+//Note
+//If there is nothing to sum, the sum is default to 0.
+
+
+
+export function positiveSum(arr:number[]| null):number {
+    if(arr === null) return 0;
+    let total:number = 0;
+
+    for(let i = 0; i<arr.length;i++){
+        if(arr[i] > 0){ total+= arr[i]}
+    }
+
+  return total;
+}
